39053a2ab80abc5abfc25c11e32dc0fd369e927f
Ws-Peroth <a01033056809@gmail.com>
2021-05-15 21:03:57 +0900

    Update EventManager.cs
    Add EventListener

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using Photon.Chat;
+using ExitGames.Client.Photon;
 
-
-
-public class EventManager : MonoBehaviour
+public class EventManager : MonoBehaviour, IOnEventCallback
 {
     public static EventManager eventManager;
+
+    public void EventListener(EventData photonEvent)
+    {
+        ((IOnEventCallback)eventManager).EventListener(photonEvent);
+    }
 
     // Start is called before the first frame update
     void Start()
